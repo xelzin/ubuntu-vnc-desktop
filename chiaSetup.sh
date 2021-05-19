512fc050fbd05eb30222d04e05b5e3bf2de88c45
Ryan Warwick <spdshadow_w@yahoo.com>
2021-05-20 02:33:23 +0800

    Fixed minor typo in chiaSetup.sh

--- chiaSetup.sh
+++ chiaSetup.sh
@@ -2,16 +2,16 @@
 cd /chia-blockchain
 . ./activate
 chia init
-if [ "$1" == "generate" ]; then
+if [ "$KEYS== "generate" ]; then
   echo "to use your own keys pass them as a text file -v /path/to/keyfile:/path/in/container and -e keys=\"/path/in/container\""
   chia keys generate
 else
-  chia keys add -f $1
+  chia keys add -f $KEYS
 fi
 
 if [ ! "$(ls -A /plots)" ]; then
   echo "Plots directory appears to be empty and you have not specified another, try mounting a plot directory with the docker -v command "
 fi
 
-sed -i 's/localhost/127.0.0.1/g' $2/.chia/mainnet/config/config.yaml
+sed -i 's/localhost/127.0.0.1/g' $HOME/.chia/mainnet/config/config.yaml
 
